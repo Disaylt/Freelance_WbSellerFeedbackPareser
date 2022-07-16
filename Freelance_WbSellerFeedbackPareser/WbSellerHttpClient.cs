063bf2fffca3df78c4866142a76de36223f63b00
disaylt <disofban@gmail.com>
2022-07-16 16:41:50 +0300

    create wb http clien handler

---
+++ Freelance_WbSellerFeedbackPareser/WbSellerHttpClient.cs
@@ -0,0 +1,32 @@
+﻿using Freelance_WbSellerFeedbackPareser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freelance_WbSellerFeedbackPareser
+{
+    internal class WbSellerHttpClient
+    {
+        public HttpClientHandler HttpClientHandler { get; }
+        public WbSellerHttpClient(SellerSettingsModel sellerSettingsModel)
+        {
+            HttpClientHandler = new HttpClientHandler();
+            HttpClientHandler.CookieContainer = CreateWbCookieContainer(sellerSettingsModel);
+        }
+
+        private CookieContainer CreateWbCookieContainer(SellerSettingsModel sellerSettingsModel)
+        {
+            CookieCollection cookieCollection = new CookieCollection
+            {
+                new Cookie("WBToken", sellerSettingsModel.WbToken, "/", "seller.wildberries.ru"),
+                new Cookie("x-supplier-id", sellerSettingsModel.SupplierId, "/", "seller.wildberries.ru")
+            };
+            CookieContainer cookieContainer = new CookieContainer();
+            cookieContainer.Add(cookieCollection);
+            return cookieContainer;
+        }
+    }
+}
