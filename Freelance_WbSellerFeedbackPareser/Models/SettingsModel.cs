632c1c74455fb1e70d242b27033525e10e60b511
disaylt <disofban@gmail.com>
2022-07-16 16:03:26 +0300

    create settings model

---
+++ Freelance_WbSellerFeedbackPareser/Models/SettingsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freelance_WbSellerFeedbackPareser.Models
+{
+    internal class SettingsModel
+    {
+        public string? SellerName { get; set; }
+        public string? WbToken { get; set; }
+        public string? SupplierId { get; set; }
+    }
+}
