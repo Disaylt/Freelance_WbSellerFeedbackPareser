faf47284eddaeea705b5c0bbe5d0c19ff9b43fc1
disaylt <disofban@gmail.com>
2022-08-06 11:15:53 +0300

    move SetHeaders

--- Freelance_WbSellerFeedbackPareser/WbSellerHttpClient.cs
+++ Freelance_WbSellerFeedbackPareser/WbSellerHttpClient.cs
@@ -20,6 +20,19 @@ namespace Freelance_WbSellerFeedbackPareser
 
         public HttpClientHandler HttpClientHandler { get; }
 
+        protected virtual void SetHeaders(HttpRequestMessage request)
+        {
+            request.Headers.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0");
+            request.Headers.TryAddWithoutValidation("Accept", "*/*");
+            request.Headers.TryAddWithoutValidation("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
+            request.Headers.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, br");
+            request.Headers.TryAddWithoutValidation("Sec-Fetch-Dest", "empty");
+            request.Headers.TryAddWithoutValidation("Sec-Fetch-Mode", "cors");
+            request.Headers.TryAddWithoutValidation("Sec-Fetch-Site", "same-origin");
+            request.Headers.TryAddWithoutValidation("Referer", "https://seller.wildberries.ru/service-desk-v2/requests/history");
+            request.Headers.TryAddWithoutValidation("Connection", "keep-alive");
+        }
+
         private CookieContainer CreateWbCookieContainer(SellerSettingsModel sellerSettingsModel)
         {
             CookieCollection cookieCollection = new CookieCollection
